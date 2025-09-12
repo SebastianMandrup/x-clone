889c163c388293a0affdd8e455cb05eecb4507ff
SebastianMandrup <46905237+SebastianMandrup@users.noreply.github.com>
2025-09-12 23:27:08 +0200

    started css cleanup

--- home.php
+++ home.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./app.css">
-    <script src='./app.js' defer></script>
+    <link rel="stylesheet" href="./styling/app.css">
+    <script src='./scripts/app.js' defer></script>
     <title>Home / X</title>
 </head>
 
