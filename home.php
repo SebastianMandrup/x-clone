6ce2f763f484f8b748594f0f79f5215d14e1de82
SebastianMandrup <46905237+SebastianMandrup@users.noreply.github.com>
2025-09-14 14:52:39 +0200

    minor changes to the aside search form

--- home.php
+++ home.php
@@ -688,7 +688,7 @@
         <aside>
             <section id='sectionSearch'>
                 <form action="" id='formSearch'>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"
                         fill="currentColor" aria-hidden="true" class="search-icon">
                         <title>Search</title>
                         <g>
