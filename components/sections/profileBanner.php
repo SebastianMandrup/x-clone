1e26439d63dff86d7d2f4004a770de6f0175d70d
SebastianMandrup <sebastianmandrup@outlook.com>
2025-11-19 23:18:32 +0100

    closes #16

---
+++ components/sections/profileBanner.php
@@ -0,0 +1,54 @@
+<section id='sectionProfileBanner'>
+
+	<?php if (!$user["user_banner"]) : ?>
+		<div id='divDefaultBanner'></div>
+	<?php else : ?>
+		<img id='imgProfileBanner' src="<?php muoEcho($user["user_banner"]); ?>" alt="Profile Banner Image">
+	<?php endif; ?>
+	<section id='sectionUserDetails'>
+		<img id='imgUserAvatarBanner' src="https://ui-avatars.com/api/?name=<?php muoEcho($user['user_name']); ?>&background=random"
+			alt="Avatar">
+
+		<?php if ($user["user_pk"] === $_SESSION["user"]["user_pk"]) : ?>
+			<button id='buttonEditProfile' onclick="location.href='/settings/profile';">
+				Edit profile
+			</button>
+		<?php else : ?>
+			<button id='buttonFollow'>
+				Follow
+			</button>
+		<?php endif; ?>
+	</section>
+	<section id='sectionUserData'>
+		<h2 id='h2UserNameBanner'>
+			<?php muoEcho($user["user_name"]); ?>
+		</h2>
+		<p id='pUserHandleBanner'>
+			@<?php muoEcho($user["user_handle"]); ?>
+		</p>
+		<p id='pUserBioBanner'>
+			<?php
+			if (isset($user["user_bio"]) && !empty($user["user_bio"])) {
+				muoEcho($user["user_bio"]);
+			} else {
+				muoEcho("This user has not set a bio yet.");
+			}
+			?>
+		</p>
+		<section id='sectionUserFollowingBanner'>
+			<p class='pFollowCountBanner'>
+				<span>
+					<?php muoEcho($user["following_count"]); ?>
+				</span>
+				Following
+			</p>
+			<p class='pFollowCountBanner'>
+				<span>
+					<?php muoEcho($user["followers_count"]); ?>
+				</span>
+				Followers
+			</p>
+		</section>
+
+	</section>
+</section>
--- views/profile.php
+++ views/profile.php
@@ -17,11 +17,14 @@ $sql = "
     users.user_email, 
     users.user_birthday, 
     users.user_handle,
-    COUNT(posts.post_pk) AS post_count
+    users.user_banner,
+    COUNT(posts.post_pk) AS post_count,
+    (SELECT COUNT(*) FROM follows WHERE followed_user_fk = users.user_pk AND follow_deleted_at = 0) AS followers_count,
+    (SELECT COUNT(*) FROM follows WHERE following_user_fk = users.user_pk AND follow_deleted_at = 0) AS following_count
 	FROM users 
 	LEFT JOIN posts ON users.user_pk = posts.post_user_fk
 	WHERE users.user_handle = :userHandle
-	GROUP BY users.user_pk, users.user_name, users.user_email, users.user_birthday, users.user_handle
+	GROUP BY users.user_pk, users.user_name, users.user_email, users.user_birthday, users.user_handle, users.user_banner
 	LIMIT 1;
 ";
 $stmt = $_db->prepare($sql);
@@ -57,6 +60,71 @@ if (!$user) {
 		<main>
 
 			<?php require_once __DIR__ . '/../components/sections/profileHeader.php'; ?>
+			<?php require_once __DIR__ . '/../components/sections/profileBanner.php'; ?>
+
+
+			<?php
+			require_once __DIR__ . '../../db_connector.php';
+
+
+			$sql = "SELECT 
+                    p.post_pk,
+                    p.post_content,
+                    p.post_image,
+                    p.post_created_at,
+                    u.user_pk,
+                    u.user_name,
+                    u.user_handle,
+                    
+                    -- referenced post fields
+                    rp.post_pk AS ref_post_pk,
+                    rp.post_content AS ref_post_content,
+                    rp.post_image AS ref_post_image,
+                    rp.post_created_at AS ref_post_created_at,
+                    ru.user_pk AS ref_user_pk,
+                    ru.user_name AS ref_user_name,
+                    ru.user_handle AS ref_user_handle,
+
+                    -- like information
+                    phl.user_fk AS liked_by_user,
+                    phl.like_deleted_at,
+                    -- like count (no alias needed in subquery)
+                    (SELECT COUNT(*) FROM likes WHERE post_fk = p.post_pk AND like_deleted_at IS NULL) AS like_count,
+
+                    -- comment count
+                    (SELECT COUNT(*) FROM comments WHERE comment_post_fk = p.post_pk AND comment_deleted_at IS NULL) AS comment_count
+
+                    FROM posts p 
+                    INNER JOIN users u 
+                        ON p.post_user_fk = u.user_pk
+
+                    -- self join to bring in the referenced post
+                    LEFT JOIN posts rp 
+                        ON p.post_reference = rp.post_pk
+                    LEFT JOIN users ru 
+                        ON rp.post_user_fk = ru.user_pk
+
+                    -- check if current user liked this post
+                    LEFT JOIN likes phl 
+                        ON p.post_pk = phl.post_fk 
+                        AND phl.user_fk = :current_user_pk
+                        AND phl.like_deleted_at IS NULL
+
+					WHERE p.post_user_fk = :profile_user_pk AND p.post_deleted_at IS NULL
+                    ORDER BY p.post_created_at DESC";
+
+			$stmt = $_db->prepare($sql);
+			$stmt->bindValue(':current_user_pk', $_SESSION['user']['user_pk']);
+			$stmt->bindValue(':profile_user_pk', $user['user_pk']);
+			$stmt->execute();
+
+			$posts = $stmt->fetchAll();
+			foreach ($posts as $post) {
+				require __DIR__ . '../../components/articles/post.php';
+			}
+
+			?>
+
 
 			<div class="circle-loader"></div>
 		</main>
