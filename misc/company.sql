6e8aeab0339aab52e50184715288fec581d37663
SebastianMandrup <sebastianmandrup@outlook.com>
2025-11-22 02:10:51 +0100

    updated company.sql

--- misc/company.sql
+++ misc/company.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mariadb
--- Generation Time: Nov 21, 2025 at 03:54 PM
+-- Generation Time: Nov 22, 2025 at 01:10 AM
 -- Server version: 10.6.20-MariaDB-ubu2004
 -- PHP Version: 8.3.26
 
@@ -42,8 +42,12 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`comment_pk`, `comment_user_fk`, `comment_post_fk`, `comment_content`, `comment_created_at`, `comment_updated_at`, `comment_deleted_at`) VALUES
+('007334d23995a7d1dc0b4c372bdac2f4d81bec702c57db0af4', '12345', 'c4cc166eed2dd30c6c9ecc328be7752e80c2966f86a627e1cb', 'test', 1763758853, 1763758853, NULL),
+('0c4ebb8a3a5995d60e9813e5fe5591f78a246785273bc2ecab', '12345', '45bef48541cfcfe47fc94b481f2780a555f9d625324401e2e2', '123', 1763758893, 1763758893, NULL),
 ('0d6c413259a98428902eb030b28550e4cc23cc0ae99fce8e81', '12345', 'c6e50efff3d7e11635601b0cb21f4be072dcc0368b0657f61a', 'asdasd', 1763740408, 1763740408, NULL),
 ('0f2ab90a6ea70b74999246b7c081918510ef926489ecc26cee', '12345', '0084814eae606a8c1cbbc8595409616491e8fb0a059c845162', 'test', 1763738902, 1763738902, NULL),
+('14de9fa848678c233f227c35cecdc392e3daa922498d858294', '12345', 'e93aaaee9bf7c84e5fa5bedff3cce1c12baa6911abf5baffdf', 'asdasd', 1763759142, 1763759142, NULL),
+('15f36dd11b3069cb620d0e7c24c58b91dca8b1a4982ea33d73', '12345', 'c4cc166eed2dd30c6c9ecc328be7752e80c2966f86a627e1cb', 'test', 1763758837, 1763758837, NULL),
 ('1c9098b90a5b83b036aaa49ff3c67452bf204c7c1100d683ea', '12345', '0e398a9a2ec1970af1e171280bd895bce936544b89ec9323b4', '!!!', 1763505193, 1763505193, NULL),
 ('288870c55736efc2db58ab1554327bcf71551f3ac4a0ab1e5d', '12345', '1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', 'actual new comment', 1763487060, 1763487060, NULL),
 ('2e2f5b9459b56a4892e8f643b57eaa7d31a0511f59a7d3d04e', '12345', '1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', '!!!', 1763505193, 1763505193, NULL),
@@ -51,6 +55,7 @@ INSERT INTO `comments` (`comment_pk`, `comment_user_fk`, `comment_post_fk`, `com
 ('36f34eb31532457332b6e720943ff0f293ab0dcf8cf2bbae35', '12345', '1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', '!!!!', 1763505102, 1763505102, NULL),
 ('3f70c1e03e18fc1604940cfc2de8277b33c16cc08a7880850e', '12345', '1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', '!', 1763505204, 1763505204, NULL),
 ('412ad0ae18f5e81da99ad213b360766d6b7a6ce012f2f97893', '12345', '1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', '123123', 1763485484, 1763485484, NULL),
+('485956768be4a2c880cc0431e7b5cf1cebb8dad96907d744e3', '12345', 'c4cc166eed2dd30c6c9ecc328be7752e80c2966f86a627e1cb', 'test', 1763758837, 1763758837, NULL),
 ('4a982bcef54f9fd5deafbaa23d5915f35d325863d149ec61e3', '12345', '1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', 'new comment', 1763487018, 1763487018, NULL),
 ('66d6097619b72d308025173e606848f4824118f5c68c19b349', '12345', '0084814eae606a8c1cbbc8595409616491e8fb0a059c845162', 'test', 1763738902, 1763738902, NULL),
 ('6a993aa4c3d9e4aaec7cd559f75f286e44c76d04f10efba6d0', '12345', '1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', '!!!', 1763505208, 1763505208, NULL),
@@ -65,8 +70,29 @@ INSERT INTO `comments` (`comment_pk`, `comment_user_fk`, `comment_post_fk`, `com
 ('af43b2f61194148f35ea4589b94d0b48b898be7f5aeb816502', '12345', '1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', '123123', 1763485484, 1763485484, NULL),
 ('c1b94a701741d6564ea4bb13f5e751f97fba1fdac18cac030e', '12345', '1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', '!!!', 1763505208, 1763505208, NULL),
 ('d56395fd6fd5042c31985ba9c412f41b5051e48cd2037a6cb0', '12345', '1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', 'new comment', 1763486994, 1763486994, NULL),
+('dee8a813d1869ee6dcc0a3bc390dce67e38663f173e9e53c60', '12345', 'c4cc166eed2dd30c6c9ecc328be7752e80c2966f86a627e1cb', '123', 1763770907, 1763770907, NULL),
 ('e55f997707106b726d90e9734518b398b4d448cd0b824f85df', '1234561', '468122919d41804f57b1f806de4998d6e8d1cff0da0d59ef9a', 'testing comments', 1763662074, 1763662074, NULL),
 ('ec6a89e5d969d770d2938155de5dd514256d8ff6b523af60b4', '12345', '1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', 'new comment', 1763486994, 1763486994, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment_likes`
+--
+
+CREATE TABLE `comment_likes` (
+  `comment_fk` char(50) NOT NULL,
+  `user_fk` char(50) NOT NULL,
+  `like_created_at` bigint(20) UNSIGNED NOT NULL,
+  `like_deleted_at` bigint(20) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `comment_likes`
+--
+
+INSERT INTO `comment_likes` (`comment_fk`, `user_fk`, `like_created_at`, `like_deleted_at`) VALUES
+('14de9fa848678c233f227c35cecdc392e3daa922498d858294', '12345', 1763771804, NULL);
 
 -- --------------------------------------------------------
 
@@ -87,56 +113,12 @@ CREATE TABLE `follows` (
 
 INSERT INTO `follows` (`following_user_fk`, `followed_user_fk`, `follow_created_at`, `follow_deleted_at`) VALUES
 ('12345', '12345', 1763729027, 1763729028),
+('12345', '1234561', 1763754106, 1763754141),
 ('12345', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763729078, NULL),
 ('1234561', '12345', 1763661481, 1763681610),
 ('1234561', '1234561', 1763678339, 1763678340),
 ('1234561', '3fc4649a70682d05a078952128a7d7b8eca54cc102be4dd10a', 1763678347, 1763678347),
 ('1234561', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763661400, 1763661737);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `post_likes`
---
-
-CREATE TABLE `post_likes` (
-  `post_fk` char(50) NOT NULL,
-  `user_fk` char(50) NOT NULL,
-  `like_created_at` bigint(20) UNSIGNED NOT NULL,
-  `like_deleted_at` bigint(20) UNSIGNED DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `post_likes`
---
-
-INSERT INTO `post_likes` (`post_fk`, `user_fk`, `like_created_at`, `like_deleted_at`) VALUES
-('0084814eae606a8c1cbbc8595409616491e8fb0a059c845162', '12345', 1763738384, 1763738895),
-('0e398a9a2ec1970af1e171280bd895bce936544b89ec9323b4', '12345', 1763478296, 1763498301),
-('0e398a9a2ec1970af1e171280bd895bce936544b89ec9323b4', '1234561', 1763662057, 1763691548),
-('0e398a9a2ec1970af1e171280bd895bce936544b89ec9323b4', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567769, NULL),
-('1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', '12345', 1763477359, 1763737881),
-('1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567730, NULL),
-('45bef48541cfcfe47fc94b481f2780a555f9d625324401e2e2', '1234561', 1763685732, NULL),
-('45bef48541cfcfe47fc94b481f2780a555f9d625324401e2e2', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567727, NULL),
-('468122919d41804f57b1f806de4998d6e8d1cff0da0d59ef9a', '12345', 1763478297, 1763498129),
-('468122919d41804f57b1f806de4998d6e8d1cff0da0d59ef9a', '1234561', 1763662063, 1763691547),
-('468122919d41804f57b1f806de4998d6e8d1cff0da0d59ef9a', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567770, NULL),
-('69fa74d9b6973c336eeb5d6cfa3ad7b1d78e19ccc964afd301', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567729, NULL),
-('b163d6c2708fb7b3802f1f0dc1c07f3314bf6ce84b7b2f0db6', '12345', 1763478300, 1763478307),
-('b163d6c2708fb7b3802f1f0dc1c07f3314bf6ce84b7b2f0db6', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567771, NULL),
-('c4cc166eed2dd30c6c9ecc328be7752e80c2966f86a627e1cb', '12345', 1763590581, 1763590586),
-('c4cc166eed2dd30c6c9ecc328be7752e80c2966f86a627e1cb', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567913, 1763567914),
-('c6e50efff3d7e11635601b0cb21f4be072dcc0368b0657f61a', '12345', 1763740398, 1763740398),
-('d6e8549a3b1bd903a6ee2546fa4836a75e5c790db8bee8a572', '12345', 1763740280, NULL),
-('d6e8549a3b1bd903a6ee2546fa4836a75e5c790db8bee8a572', '1234561', 1763685263, NULL),
-('d6e8549a3b1bd903a6ee2546fa4836a75e5c790db8bee8a572', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567728, NULL),
-('e32dd06cb4fc1ac44565795f9f6dccff2b91a723eb032db20e', '12345', 1763577029, NULL),
-('e32dd06cb4fc1ac44565795f9f6dccff2b91a723eb032db20e', '1234561', 1763679709, NULL),
-('f55ae31185245855d93a2d195c3289ced3e311728463288785', '12345', 1763577028, 1763729563),
-('f55ae31185245855d93a2d195c3289ced3e311728463288785', '1234561', 1763685259, 1763691761),
-('f55ae31185245855d93a2d195c3289ced3e311728463288785', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763569934, 1763571872),
-('fb999bf4bcdaedcbf4c1dffd5a0e44b1a6ef9d656911471083', '12345', 1763478301, 1763737889);
 
 -- --------------------------------------------------------
 
@@ -162,17 +144,21 @@ INSERT INTO `posts` (`post_pk`, `post_content`, `post_image`, `post_reference`, 
 ('0084814eae606a8c1cbbc8595409616491e8fb0a059c845162', NULL, NULL, 'c4cc166eed2dd30c6c9ecc328be7752e80c2966f86a627e1cb', '12345', 1763738162, NULL),
 ('0e398a9a2ec1970af1e171280bd895bce936544b89ec9323b4', 'sick post', 'https://pbs.twimg.com/profile_images/1975532666242949120/63VlQ3ml_400x400.jpg', '468122919d41804f57b1f806de4998d6e8d1cff0da0d59ef9a', '1234561', 1758677913, NULL),
 ('1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', 'new post', NULL, NULL, '12345', 1763474461, NULL),
+('445cd0cc965d3a763085d3124e0812684aea96f3a481dc5a10', NULL, NULL, 'f55ae31185245855d93a2d195c3289ced3e311728463288785', '12345', 1763749412, NULL),
 ('45bef48541cfcfe47fc94b481f2780a555f9d625324401e2e2', '11111111', NULL, NULL, 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763565440, NULL),
 ('468122919d41804f57b1f806de4998d6e8d1cff0da0d59ef9a', 'testing again', NULL, NULL, '1234561', 1758676513, NULL),
 ('69fa74d9b6973c336eeb5d6cfa3ad7b1d78e19ccc964afd301', 'tests', 'https://pbs.twimg.com/profile_images/1975532666242949120/63VlQ3ml_400x400.jpg', NULL, '12345', 1763505820, NULL),
 ('73a99d4731940af45da1d495b46b1ca16980e46aacb95cf906', NULL, NULL, 'd6e8549a3b1bd903a6ee2546fa4836a75e5c790db8bee8a572', '12345', 1763740247, NULL),
+('a44d9d1b9f4841b72d89e33d0c4082546ca987be5a31e27b58', NULL, NULL, 'c4cc166eed2dd30c6c9ecc328be7752e80c2966f86a627e1cb', '12345', 1763740888, NULL),
 ('a76dbb68f17dadf0a11fc8fbb514c977ea5df750017d2a0852', NULL, NULL, 'c4cc166eed2dd30c6c9ecc328be7752e80c2966f86a627e1cb', '12345', 1763739721, NULL),
 ('b163d6c2708fb7b3802f1f0dc1c07f3314bf6ce84b7b2f0db6', 'test three', NULL, NULL, '1234561', 1758676513, NULL),
 ('c4cc166eed2dd30c6c9ecc328be7752e80c2966f86a627e1cb', 'asdasdasd', NULL, NULL, 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567778, NULL),
 ('c6e50efff3d7e11635601b0cb21f4be072dcc0368b0657f61a', NULL, NULL, 'c4cc166eed2dd30c6c9ecc328be7752e80c2966f86a627e1cb', '12345', 1763739705, NULL),
 ('d6e8549a3b1bd903a6ee2546fa4836a75e5c790db8bee8a572', '123123123123', NULL, NULL, '12345', 1763565353, NULL),
 ('e32dd06cb4fc1ac44565795f9f6dccff2b91a723eb032db20e', 'adfgdafgdafgdafg', NULL, NULL, 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567780, NULL),
+('e93aaaee9bf7c84e5fa5bedff3cce1c12baa6911abf5baffdf', NULL, NULL, '69fa74d9b6973c336eeb5d6cfa3ad7b1d78e19ccc964afd301', '12345', 1763757774, NULL),
 ('f55ae31185245855d93a2d195c3289ced3e311728463288785', 'dafgdafgdafgdfag', NULL, NULL, 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567782, NULL),
+('f9b2b963bcb6c8fce989a22edff6d0a466ec196ebef4d554c4', NULL, NULL, 'f55ae31185245855d93a2d195c3289ced3e311728463288785', '12345', 1763749411, NULL),
 ('fb999bf4bcdaedcbf4c1dffd5a0e44b1a6ef9d656911471083', 'test', NULL, NULL, '1234561', 1758676513, NULL);
 
 --
@@ -182,6 +168,55 @@ DELIMITER $$
 CREATE TRIGGER `before_insert_post` BEFORE INSERT ON `posts` FOR EACH ROW SET NEW.post_created_at = UNIX_TIMESTAMP()
 $$
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_likes`
+--
+
+CREATE TABLE `post_likes` (
+  `post_fk` char(50) NOT NULL,
+  `user_fk` char(50) NOT NULL,
+  `like_created_at` bigint(20) UNSIGNED NOT NULL,
+  `like_deleted_at` bigint(20) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `post_likes`
+--
+
+INSERT INTO `post_likes` (`post_fk`, `user_fk`, `like_created_at`, `like_deleted_at`) VALUES
+('0084814eae606a8c1cbbc8595409616491e8fb0a059c845162', '12345', 1763738384, 1763738895),
+('0e398a9a2ec1970af1e171280bd895bce936544b89ec9323b4', '12345', 1763478296, 1763498301),
+('0e398a9a2ec1970af1e171280bd895bce936544b89ec9323b4', '1234561', 1763662057, 1763691548),
+('0e398a9a2ec1970af1e171280bd895bce936544b89ec9323b4', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567769, NULL),
+('1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', '12345', 1763477359, 1763737881),
+('1b8e98197a9301a10e5f5824cb704aaa447186ec84ef7d87c7', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567730, NULL),
+('445cd0cc965d3a763085d3124e0812684aea96f3a481dc5a10', '12345', 1763755572, 1763770238),
+('45bef48541cfcfe47fc94b481f2780a555f9d625324401e2e2', '12345', 1763770653, NULL),
+('45bef48541cfcfe47fc94b481f2780a555f9d625324401e2e2', '1234561', 1763685732, NULL),
+('45bef48541cfcfe47fc94b481f2780a555f9d625324401e2e2', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567727, NULL),
+('468122919d41804f57b1f806de4998d6e8d1cff0da0d59ef9a', '12345', 1763478297, 1763498129),
+('468122919d41804f57b1f806de4998d6e8d1cff0da0d59ef9a', '1234561', 1763662063, 1763691547),
+('468122919d41804f57b1f806de4998d6e8d1cff0da0d59ef9a', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567770, NULL),
+('69fa74d9b6973c336eeb5d6cfa3ad7b1d78e19ccc964afd301', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567729, NULL),
+('b163d6c2708fb7b3802f1f0dc1c07f3314bf6ce84b7b2f0db6', '12345', 1763478300, 1763478307),
+('b163d6c2708fb7b3802f1f0dc1c07f3314bf6ce84b7b2f0db6', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567771, NULL),
+('c4cc166eed2dd30c6c9ecc328be7752e80c2966f86a627e1cb', '12345', 1763590581, NULL),
+('c4cc166eed2dd30c6c9ecc328be7752e80c2966f86a627e1cb', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567913, 1763567914),
+('c6e50efff3d7e11635601b0cb21f4be072dcc0368b0657f61a', '12345', 1763740398, 1763740398),
+('d6e8549a3b1bd903a6ee2546fa4836a75e5c790db8bee8a572', '12345', 1763740280, NULL),
+('d6e8549a3b1bd903a6ee2546fa4836a75e5c790db8bee8a572', '1234561', 1763685263, NULL),
+('d6e8549a3b1bd903a6ee2546fa4836a75e5c790db8bee8a572', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763567728, NULL),
+('e32dd06cb4fc1ac44565795f9f6dccff2b91a723eb032db20e', '12345', 1763577029, 1763770648),
+('e32dd06cb4fc1ac44565795f9f6dccff2b91a723eb032db20e', '1234561', 1763679709, NULL),
+('e93aaaee9bf7c84e5fa5bedff3cce1c12baa6911abf5baffdf', '12345', 1763758740, NULL),
+('f55ae31185245855d93a2d195c3289ced3e311728463288785', '12345', 1763577028, NULL),
+('f55ae31185245855d93a2d195c3289ced3e311728463288785', '1234561', 1763685259, 1763691761),
+('f55ae31185245855d93a2d195c3289ced3e311728463288785', 'b48f57e0506656c0701066ec12d116e45e2a02b60f8cf1ddea', 1763569934, 1763571872),
+('f9b2b963bcb6c8fce989a22edff6d0a466ec196ebef4d554c4', '12345', 1763770204, 1763770274),
+('fb999bf4bcdaedcbf4c1dffd5a0e44b1a6ef9d656911471083', '12345', 1763478301, 1763737889);
 
 -- --------------------------------------------------------
 
@@ -274,18 +309,18 @@ ALTER TABLE `comments`
   ADD KEY `user_fk` (`comment_user_fk`);
 
 --
+-- Indexes for table `comment_likes`
+--
+ALTER TABLE `comment_likes`
+  ADD PRIMARY KEY (`comment_fk`,`user_fk`),
+  ADD KEY `user_fk` (`user_fk`);
+
+--
 -- Indexes for table `follows`
 --
 ALTER TABLE `follows`
   ADD PRIMARY KEY (`following_user_fk`,`followed_user_fk`),
   ADD KEY `followed_user_fk` (`followed_user_fk`);
-
---
--- Indexes for table `post_likes`
---
-ALTER TABLE `post_likes`
-  ADD PRIMARY KEY (`post_fk`,`user_fk`),
-  ADD KEY `user_fk` (`user_fk`);
 
 --
 -- Indexes for table `posts`
@@ -294,6 +329,13 @@ ALTER TABLE `posts`
   ADD PRIMARY KEY (`post_pk`),
   ADD KEY `post_user_fk` (`post_user_fk`),
   ADD KEY `post_reference` (`post_reference`);
+
+--
+-- Indexes for table `post_likes`
+--
+ALTER TABLE `post_likes`
+  ADD PRIMARY KEY (`post_fk`,`user_fk`),
+  ADD KEY `user_fk` (`user_fk`);
 
 --
 -- Indexes for table `topics`
@@ -325,6 +367,13 @@ ALTER TABLE `comments`
   ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`comment_user_fk`) REFERENCES `users` (`user_pk`) ON DELETE CASCADE;
 
 --
+-- Constraints for table `comment_likes`
+--
+ALTER TABLE `comment_likes`
+  ADD CONSTRAINT `comment_likes_ibfk_1` FOREIGN KEY (`comment_fk`) REFERENCES `comments` (`comment_pk`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comment_likes_ibfk_2` FOREIGN KEY (`user_fk`) REFERENCES `users` (`user_pk`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `follows`
 --
 ALTER TABLE `follows`
@@ -332,18 +381,18 @@ ALTER TABLE `follows`
   ADD CONSTRAINT `follows_ibfk_2` FOREIGN KEY (`following_user_fk`) REFERENCES `users` (`user_pk`) ON DELETE CASCADE;
 
 --
--- Constraints for table `post_likes`
---
-ALTER TABLE `post_likes`
-  ADD CONSTRAINT `post_likes_ibfk_1` FOREIGN KEY (`post_fk`) REFERENCES `posts` (`post_pk`) ON DELETE CASCADE,
-  ADD CONSTRAINT `post_likes_ibfk_2` FOREIGN KEY (`user_fk`) REFERENCES `users` (`user_pk`) ON DELETE CASCADE;
-
---
 -- Constraints for table `posts`
 --
 ALTER TABLE `posts`
   ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`post_user_fk`) REFERENCES `users` (`user_pk`) ON DELETE CASCADE,
   ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`post_reference`) REFERENCES `posts` (`post_pk`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `post_likes`
+--
+ALTER TABLE `post_likes`
+  ADD CONSTRAINT `post_likes_ibfk_1` FOREIGN KEY (`post_fk`) REFERENCES `posts` (`post_pk`) ON DELETE CASCADE,
+  ADD CONSTRAINT `post_likes_ibfk_2` FOREIGN KEY (`user_fk`) REFERENCES `users` (`user_pk`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
